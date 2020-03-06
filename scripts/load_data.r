44bfd1fedb290e65506f007470d49348b05d8c75
stannam <stan.nam98@gmail.com>
2020-03-05 18:48:47 -0800

    Task 2.1: Write a script to load in your data.

--- scripts/load_data.r
+++ scripts/load_data.r
@@ -3,16 +3,18 @@
 
 "This script loads in a dataset and saves it in the 'data' directory. This script takes the raw data URL as the variable argument.
 
-Usage: load_data.R <data_url>
+Usage: load_data.R --data_url=<data_url>
 " -> doc
 
+library(readr)
 library(docopt)
 
 opt <- docopt(doc)
 
 main <- function(data_url){
-  target_dir = ""
+  print(paste0("Downloading data file from: ", data_url))
   load_from_url (data_url, target_dir)
+  print(paste0("Successful! The file lives in ", here::here("data")))
 }
 
 #' load data from the URL to raw data 'data_url'
@@ -22,7 +24,9 @@ main <- function(data_url){
 #' @examples
 #' load_from_url('https://github.com/STAT547-UBC-2019-20/data_sets/raw/master/fire_archive_M6_96619.csv')
 load_from_url <- function (data_url, target_dir) { 
-  print(data_url)
+  target_dir <- here::here("data","fire_archive_M6_96619.csv")
+  downloaded_csv <- read_csv(data_url)
+  write_csv(downloaded_csv,target_dir)
 }  
 
 main(opt$data_url)
