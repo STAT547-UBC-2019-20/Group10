c3f2750fa58fef744ab2067142366a651a38cfec
Xuemeng Li <luckymeng7@gmail.com>
2020-03-14 16:36:49 -0700

    Add test.R

---
+++ tests/tests.R
@@ -0,0 +1,17 @@
+# Author: Xuemeng Li
+# Date: March 14, 2020
+
+library(testthat)
+context("Test the output files through make pipeline")
+
+testthat::test_that("All required files through the pipeline are generated",{
+  expect_equal(file.exists(here::here("images", "correllgram.png")), TRUE)
+  expect_equal(file.exists(here::here("images", "effectSizes.png")), TRUE)
+  expect_equal(file.exists(here::here("images", "geogram.png")), TRUE)
+  expect_equal(file.exists(here::here("images", "scan-daynight.png")), TRUE)
+  expect_equal(file.exists(here::here("images", "track-daynight.png")), TRUE)
+  expect_equal(file.exists(here::here("data", "cleaned_data.csv")), TRUE)
+  expect_equal(file.exists(here::here("data", "fire_archive_M6_96619.csv")), TRUE)
+  expect_equal(file.exists(here::here("models.rda")), TRUE)
+}
+)
