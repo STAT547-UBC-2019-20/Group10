62eceba147ea75261d3b3a1f8778c2fb8255626f
Xuemeng Li <luckymeng7@gmail.com>
2020-03-27 19:41:20 -0700

    Create the tab for the website

--- app.R
+++ app.R
@@ -10,9 +10,44 @@ Usage: app.R
 library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
-library(tidyverse)
-library(plotly)
+suppressPackageStartupMessages(library(plotly))
+suppressPackageStartupMessages(library(tidyverse))
 
+## Functions 
+
+## Assign components to variables
+heading_title <- htmlH1('Austrilia Wildfire Analysis')
+
+dropdown_list <- dccDropdown(
+  options=list(
+    list(label = "Day", value = "Day"),
+    list(label = "Night", value = "Night")
+  ),
+  value = "Day"
+)
+
+## Specify layout elements
+div_header <- htmlDiv(
+  list(
+    heading_title
+  ),
+  style = list(
+    backgroundColor = '#008420', 
+    textAlign = 'center',
+    color = 'white',
+    margin = 5,
+    marginTop = 0
+  )
+)
+
+div_tabs <- htmlDiv(
+  list(
+  dccTabs(id="tabs", value='tab-1', children=list(
+    dccTab(label='Map', value='tab-1'),
+    dccTab(label='Analysis', value='tab-2')
+  )),
+  htmlDiv(id='tabs-content')
+))
 
 # 2. Create Dash instance
 app <- Dash$new()
@@ -21,27 +56,30 @@ app <- Dash$new()
 app$layout(
   htmlDiv(
     list(
-      htmlH1('Austrilia Wildfire Analysis'),
-      dccRadioItems(
-        options =list(
-          list(label = "Map", value = "Map"),
-          list(label = "Analysis", value = "Analysis" )
-        ),
-        value = "Map"
-      ),
-      dccDropdown(
-        options=list(
-          list(label = "Day", value = "Day"),
-          list(label = "Night", value = "Night")
-        ),
-        value = "Day"
-      ),
-      htmlImg(src = "https://github.com/STAT547-UBC-2019-20/Group10/raw/master/images/geogram.png", 
-              style=list( "max-width" = "80%", height = "auto", "margin-left" = "auto", "margin-right" = "auto", display = "block"))
+      div_header,
+      div_tabs
     )
   )
 )
 
+## App Callbacks
+
+# Switch between tabs
+app$callback(
+  output('tabs-content', 'children'),
+  params = list(input('tabs', 'value')),
+  function(tab){
+     if(tab == 'tab-1'){
+       return(htmlDiv(list(
+         htmlImg(src = "https://github.com/STAT547-UBC-2019-20/Group10/raw/master/images/geogram.png", 
+                 style=list( "max-width" = "80%", height = "auto", "margin-left" = "auto", "margin-right" = "auto", display = "block"))
+       )))}
+     else if(tab == 'tab-2'){
+       return(htmlDiv(list(
+         htmlH3('Tab content 2')
+       )))}
+   }
+)
 
 # 4. Run app
 app$run_server(debug = TRUE)
